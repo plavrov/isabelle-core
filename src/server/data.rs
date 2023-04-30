b41abb9eee418ed2ebfd295564f200c3ee4e5b82
Maxim Menshikov <maxim@menshikov.org>
2023-04-30 03:11:16 +0100

    Data: add schedule entries

--- src/server/data.rs
+++ src/server/data.rs
@@ -1,3 +1,4 @@
+use crate::data_model::schedule_entry::ScheduleEntry;
 use crate::data_model::mentee::*;
 use crate::data_model::user::*;
 
@@ -10,6 +11,10 @@ pub struct Data {
 
     pub mentee_cnt: u64,
     pub mentees: HashMap<u64, Mentee>,
+
+    pub schedule_entry_cnt: u64,
+    pub schedule_entries: HashMap<u64, ScheduleEntry>,
+    pub schedule_entry_times: HashMap<u64, Vec<u64>>
 }
 
 impl Data {
@@ -20,6 +25,11 @@ impl Data {
 
             mentee_cnt: 0,
             mentees: HashMap::new(),
+
+            schedule_entry_cnt: 0,
+            schedule_entries: HashMap::new(),
+
+            schedule_entry_times: HashMap::new(),
         }
     }
 }
