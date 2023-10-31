88c8e119a246615ef30c247a7aef369c67e744e7
Maxim Menshikov <maxim@menshikov.org>
2023-10-31 19:41:02 +0000

    Settings: use POST

--- src/server/setting.rs
+++ src/server/setting.rs
@@ -1,3 +1,6 @@
+use isabelle_dm::data_model::process_result::ProcessResult;
+use futures_util::TryStreamExt;
+use actix_multipart::Multipart;
 use isabelle_dm::data_model::item::Item;
 use std::ops::Deref;
 
@@ -19,7 +22,8 @@ use std::ops::DerefMut;
 pub async fn setting_edit(
     user: Identity,
     data: web::Data<State>,
-    _req: HttpRequest,
+    req: HttpRequest,
+    mut payload: Multipart,
 ) -> HttpResponse {
     let mut srv = data.server.lock().unwrap();
     let usr = get_user(srv.deref(), user.id().unwrap());
@@ -28,12 +32,31 @@ pub async fn setting_edit(
         return HttpResponse::Forbidden().into();
     }
 
-    let config = Config::new(10, false);
-    let c: Item = config.deserialize_str(&_req.query_string()).unwrap();
-    srv.settings = c.clone();
-    info!("Setting edit: {}", serde_json::to_string(&c.strs).unwrap());
+    let mut itm = serde_qs::from_str::<Item>(&req.query_string()).unwrap();
+
+    while let Ok(Some(mut field)) = payload.try_next().await {
+        while let Ok(Some(chunk)) = field.try_next().await {
+            let data = chunk;
+
+            if field.name() == "item" {
+                let v = &data.to_vec();
+                let strv = std::str::from_utf8(v).unwrap_or("{}");
+                let new_itm: Item = serde_json::from_str(strv).unwrap_or(Item::new());
+                itm.merge(&new_itm);
+            }
+        }
+    }
+
+    srv.settings = itm.clone();
+    info!("Settings edited");
     write_data(srv.deref_mut());
-    HttpResponse::Ok().into()
+    return HttpResponse::Ok().body(
+        serde_json::to_string(&ProcessResult {
+            succeeded: true,
+            error: "".to_string(),
+        })
+        .unwrap(),
+    );
 }
 
 pub async fn setting_list(
