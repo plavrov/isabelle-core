baba348217cb21bd5d04318b984e3f8035e80f71
Maxim Menshikov <maxim@menshikov.org>
2023-10-31 02:08:49 +0000

    [#19] Add method to clear OTP

--- src/server/user_control.rs
+++ src/server/user_control.rs
@@ -23,3 +23,16 @@ pub fn check_role(srv: &crate::state::data::Data, user: &Option<Item>, role: &st
         .unwrap()
         .safe_bool(&(role_is.to_owned() + role), false);
 }
+
+pub fn clear_otp(srv: &mut crate::state::data::Data, login: String) {
+    let user_col = srv.itm.get_mut("user").unwrap();
+    for item in user_col.get_all() {
+        if item.1.strs.contains_key("login") && item.1.strs["login"] == login &&
+           item.1.strs.contains_key("email") && item.1.strs["email"] == login {
+            let mut itm = item.1.clone();
+            itm.set_str("otp", "");
+            user_col.set(*item.0, itm, false);
+            return;
+        }
+    }
+}
