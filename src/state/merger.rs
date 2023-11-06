98f14607aab4f61971f0d00f4db2c3f386fc0e9e
Maxim Menshikov <maxim@menshikov.org>
2023-11-06 20:09:23 +0000

    [#6] Introduce merger

---
+++ src/state/merger.rs
@@ -0,0 +1,14 @@
+use crate::state::store::*;
+use log::info;
+
+pub async fn merge_database(st1: &mut dyn Store, st2: &mut dyn Store) {
+	let collections = st1.get_collections().await;
+	for collection in &collections {
+		info!("Merge collection: {}", &collection);
+		let items = st1.get_all_items(collection).await;
+		for item in &items {
+			info!("Setting {} item {}", &collection, &item.0);
+			st2.set_item(&collection, &item.1, false).await;
+		}
+	}
+}
