3dac75f41a802860304c2bc47baf24dd92776d41
Maxim Menshikov <maxim@menshikov.org>
2023-11-08 00:26:01 +0000

    Pass FQDN to server

--- run.sh
+++ run.sh
@@ -6,9 +6,10 @@ binary="${BINARY:-./target/debug/isabelle-core}"
 first_run="${FIRST_RUN:+--first-run}"
 port="$1"
 pub_url="$2"
-data_path="$3"
-py_path="$4"
-gc_path="$5"
+pub_fqdn="$3"
+data_path="$4"
+py_path="$5"
+gc_path="$6"
 
 if [ "$port" == "" ] ; then
 	port="8090"
@@ -16,6 +17,10 @@ fi
 
 if [ "$pub_url" == "" ] ; then
 	pub_url="http://localhost:8081"
+fi
+
+if [ "$pub_fqdn" == "" ] ; then
+	pub_fqdn="localhost"
 fi
 
 if [ "$data_path" == "" ] ; then
@@ -40,4 +45,4 @@ if [ "$gc_path" == "" ] ; then
 	gc_path="$(pwd)/isabelle-gc"
 fi
 
-RUST_LOG=info RUST_BACKTRACE=1 "${binary}" --port "${port}" --pub-url "${pub_url}" --data-path "${data_path}" --gc-path "${gc_path}" --py-path "${py_path}" ${first_run}
+RUST_LOG=info RUST_BACKTRACE=1 "${binary}" --port "${port}" --pub-url "${pub_url}" --pub-fqdn "${pub_fqdn}" --data-path "${data_path}" --gc-path "${gc_path}" --py-path "${py_path}" ${first_run}
