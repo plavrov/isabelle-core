decdeb09461adb2590577c2dbe62a9ba76dced04
Maxim Menshikov <maxim@menshikov.org>
2023-11-11 01:40:57 +0000

    Itm: don't call sort-based filter unknown

--- src/server/itm.rs
+++ src/server/itm.rs
@@ -246,14 +246,14 @@ pub async fn itm_list(user: Identity, data: web::Data<State>, req: HttpRequest) 
                 lq.collection, lq.id, lq.limit
             );
         }
-    } else if lq.id_min != u64::MAX || lq.id_max != u64::MAX {
+    } else if lq.id_min != u64::MAX || lq.id_max != u64::MAX || lq.sort_key != "" {
         lr = srv
             .rw
             .get_items(&lq.collection, lq.id_min, lq.id_max, &lq.sort_key, lq.skip, lq.limit)
             .await;
         info!(
-            "Collection {} requested range {} - {} skip {} limit {}",
-            lq.collection, lq.id_min, lq.id_max, lq.skip, lq.limit
+            "Collection {} requested range {} - {} sort {} skip {} limit {}",
+            lq.collection, lq.id_min, lq.id_max, lq.sort_key, lq.skip, lq.limit
         );
     } else if lq.id_list.len() > 0 {
         for id in lq.id_list {
