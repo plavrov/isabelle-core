3f2e27ec81444e2a77d9025340fbccfd86a88d78
Maxim Menshikov <maxim@menshikov.org>
2024-01-02 00:48:53 +0000

    Update run.sh script

--- run.sh
+++ run.sh
@@ -4,45 +4,60 @@ cd "$TOP_DIR"
 
 binary="${BINARY:-./target/debug/isabelle-core}"
 first_run="${FIRST_RUN:+--first-run}"
-port="$1"
-pub_url="$2"
-pub_fqdn="$3"
-data_path="$4"
-py_path="$5"
+port="8090"
+pub_url="http://localhost:8081"
+pub_fqdn="localhost"
+data_path="$(pwd)/data-equestrian"
+py_path=""
 gc_path="$6"
 
-if [ "$port" == "" ] ; then
-	port="8090"
+if [ "$(uname)" == "Darwin" ] ; then
+    py_path="/opt/homebrew/bin/python3"
+else
+    py_path="$(which python3)"
 fi
 
-if [ "$pub_url" == "" ] ; then
-	pub_url="http://localhost:8081"
-fi
-
-if [ "$pub_fqdn" == "" ] ; then
-	pub_fqdn="localhost"
-fi
-
-if [ "$data_path" == "" ] ; then
-	data_path="$(pwd)/sample-data"
-fi
-
-if [ "$py_path" == "" ] ; then
-	if [ "$(uname)" == "Darwin" ] ; then
-		py_path="/opt/homebrew/bin/python3"
-	else
-		py_path="$(which python3)"
-	fi
-fi
+while test -n "$1" ; do
+    case "$1" in
+        --port)
+            port="$2"
+            shift 1
+            ;;
+        --pub-url)
+            pub_url="$2"
+            shift 1
+            ;;
+        --pub-fqdn)
+            pub_fqdn="$2"
+            shift 1
+            ;;
+        --data-path)
+            data_path="$2"
+            shift 1
+            ;;
+        --py-path)
+            py_path="$2"
+            shift 1
+            ;;
+        --gc-path)
+            gc_path="$2"
+            shift 1
+            ;;
+        --first-run)
+            first_run="--first-run"
+            ;;
+    esac
+    shift 1
+done
 
 if [ "$gc_path" == "" ] ; then
-	if [ ! -d isabelle-gc ] ; then
-		git clone https://github.com/isabelle-platform/isabelle-gc.git
-		pushd isabelle-gc
-		./install.sh
-		popd
-	fi
-	gc_path="$(pwd)/isabelle-gc"
+    if [ ! -d isabelle-gc ] ; then
+        git clone https://github.com/isabelle-platform/isabelle-gc.git
+        pushd isabelle-gc
+        ./install.sh
+        popd
+    fi
+    gc_path="$(pwd)/isabelle-gc"
 fi
 
 RUST_LOG=info RUST_BACKTRACE=1 "${binary}" --port "${port}" --pub-url "${pub_url}" --pub-fqdn "${pub_fqdn}" --data-path "${data_path}" --gc-path "${gc_path}" --py-path "${py_path}" ${first_run}
