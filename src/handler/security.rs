3df1cf731d0ffb07ceb904d5b200a9079a49aa79
Maxim Menshikov <maxim@menshikov.org>
2024-01-17 23:25:53 +0000

    Add security item hook

--- src/handler/security.rs
+++ src/handler/security.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use crate::notif::email::send_email;
 use crate::server::user_control::check_role;
 use crate::state::store::Store;
@@ -187,3 +188,90 @@ pub async fn security_otp_send_email(srv: &mut crate::state::data::Data, itm: It
     )
     .await;
 }
+
+pub async fn security_itm_filter_hook(
+    mut srv: &mut crate::state::data::Data,
+    user: &Option<Item>,
+    collection: &str,
+    context: &str,
+    map: &mut HashMap<u64, Item>,
+) {
+    let mut list = true;
+    let is_admin = check_role(&mut srv, &user, "admin").await;
+
+    if is_admin && collection != "user" {
+        return;
+    }
+
+    if context == "full" {
+        list = false;
+    }
+
+    let mut short_map: HashMap<u64, Item> = HashMap::new();
+    if user.is_none() {
+        *map = short_map;
+        return;
+    }
+
+    info!(
+        "Checking collection {} user id {}",
+        collection,
+        user.as_ref().unwrap().id
+    );
+    if list {
+        for el in &mut *map {
+            if collection == "user" {
+                if *el.0 == user.as_ref().unwrap().id || is_admin {
+                    let mut itm = Item::new();
+                    itm.id = *el.0;
+                    itm.strs
+                        .insert("name".to_string(), el.1.safe_str("name", ""));
+                    if *el.0 == user.as_ref().unwrap().id || is_admin {
+                        itm.strs
+                            .insert("phone".to_string(), el.1.safe_str("phone", ""));
+                        itm.bools.insert(
+                            "has_insurance".to_string(),
+                            el.1.safe_bool("has_insurance", false),
+                        );
+                    }
+                    itm.bools.insert(
+                        "role_is_active".to_string(),
+                        el.1.safe_bool("role_is_active", false),
+                    );
+                    itm.bools.insert(
+                        "role_is_admin".to_string(),
+                        el.1.safe_bool("role_is_admin", false),
+                    );
+                    short_map.insert(*el.0, itm);
+                }
+            } else {
+                let mut itm = Item::new();
+                itm.id = *el.0;
+                itm.strs
+                    .insert("name".to_string(), el.1.safe_str("name", ""));
+                short_map.insert(*el.0, itm);
+            }
+        }
+    } else {
+        if collection == "user" {
+            for el in &mut *map {
+                if *el.0 != user.as_ref().unwrap().id && !is_admin {
+                    /* nothing */
+                } else {
+                    let mut itm = el.1.clone();
+                    if itm.strs.contains_key("salt") {
+                        itm.strs.remove("salt");
+                    }
+                    if itm.strs.contains_key("password") {
+                        itm.strs.remove("password");
+                    }
+                    short_map.insert(*el.0, itm);
+                }
+            }
+        } else {
+            short_map = map.clone();
+        }
+    }
+    *map = short_map;
+}
+
