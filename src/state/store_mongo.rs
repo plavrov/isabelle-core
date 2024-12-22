8ce0c250ae5643cf00e87e4eb46a5ec3dc992d99
Maxim Menshikov <maxim@menshikov.org>
2024-12-22 19:36:34 +0000

    Fix source [2]

--- src/state/store_mongo.rs
+++ src/state/store_mongo.rs
@@ -33,7 +33,7 @@ use log::info;
 use serde_json::Value;
 
 use mongodb::{
-    bson::doc, options::FindOptions, Client, Collection,
+    bson::doc, Client, Collection,
     IndexModel,
 };
 use std::collections::HashMap;
