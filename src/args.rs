a943e702e74b59ee44b2665180897024e3d0517d
Maxim Menshikov <maxim@menshikov.org>
2025-01-01 15:11:53 +0000

    [#44] Args: remove default comment

--- src/args.rs
+++ src/args.rs
@@ -23,7 +23,7 @@
  */
 use clap::Parser;
 
-/// Simple program to greet a person
+/// Isabelle - high-performant server for web applications
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Args {
